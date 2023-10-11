8a29e68907f0c19e43cddb6655a1dc792199185c
st117216 <vladislavkazakov1221@mail.ru>
2023-10-11 21:29:09 +0300

    cw1

---
+++ dopusk 11.10.2023/task1/task1.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+int main()
+{
+    int num = 0;
+    
+    int t = 0;
+    int d = 1;
+    int sum = 0;
+    std::cin >> num;
+    
+    while (num)
+    {
+        sum += (num % 2);
+        t += (num % 2) * d;
+        num = num / 2;
+        d = d * 10; 
+    }
+    std::cout << t << " " << sum<<std::endl;
+
+    return 0;
+}
---
+++ dopusk 11.10.2023/task1_b/task1_b.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+int main()
+{
+    int num = 0;
+    int num_i = 0;
+    int t = 0;
+    int d = 1;
+    
+    std::cin >> num;
+
+    while (num)
+    {
+        t += (num % 2) * d;
+        num = num / 2;
+        d = d * 10;
+    }
+    std::cout << t << " " << std::endl;
+
+    return 0;
+}
