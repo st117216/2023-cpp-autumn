c2cb50e3e572b86d2a144dc6d8d92898b14db990
st117216 <vladislavkazakov1221@mail.ru>
2023-10-31 12:55:23 +0300

    cw

--- 2023.10.31-peresdacha/task1/task1.cpp
+++ 2023.10.31-peresdacha/task1/task1.cpp
@@ -1,1 +1,13 @@
-﻿
+﻿#include<iostream>
+#include<math.h>
+typedef unsigned int uint;
+int main()
+{
+	const int BITS = sizeof(uint) * 8;
+	uint x = 0;
+	std::cin >> x;
+	x = ((x & 0x000000FF) << 24) | ((x & 0x0000FF00) << 8 | ((x & 0x00FF0000) >> 8) | (x & 0xFF000000) >> 24);
+	std::cout << std::endl;
+	std::cout << x;
+	return 0;
+}
