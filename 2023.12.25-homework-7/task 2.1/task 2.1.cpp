c738ee7b633aa7d52389b373b620df6e5d2efe25
st117216 <vladislavkazakov1221@mail.ru>
2023-12-25 22:14:40 +0300

    hw7

---
+++ 2023.12.25-homework-7/task 2.1/task 2.1.cpp
@@ -0,0 +1,184 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<cstdlib>
+#include <iostream>
+#include<cstring>
+#include<string>
+#include<fstream>
+
+
+
+//-----------------------------------
+char* strcat_new(char* s1, char* s2)
+{
+
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+
+	char* result = (char*)malloc(len1 + len2 + 1);
+
+	if (!result) {
+		fprintf(stderr, "malloc() failed: insufficient memory!\n");
+		return NULL;
+	}
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, len2 + 1);
+	return result;
+}
+
+char* strmult_new(char* s, int k)
+{
+	size_t len = strlen(s);
+	char* result = (char*)malloc((len * k) + 1);
+	for (int i = 0; i < k; i++)
+	{
+		memcpy(result + len * i, s, len + 1);
+
+	}
+	return result;
+}
+
+char* strstr_new(char* s, char* sub)
+{
+	int len = strlen(s);
+	int len_sub = strlen(sub);
+	char* point = NULL;
+	if (len < len_sub) {
+		printf("Error:substr>str\n");
+	}
+	else
+	{
+		for (int i = 0; i < (len - len_sub); i++)
+		{
+			if (s[i] == sub[0])
+			{
+				point = &s[i];
+				for (int j = 1; j < len_sub; j++)
+				{
+					if ((i + j) < len)
+					{
+						if ((s[i + j] != sub[j]))
+						{
+							point = NULL;
+
+						}
+					}
+				}
+			}
+		}
+	}
+	return point;
+}
+
+char* substr_new(char* s, int pos, int count)
+{
+	char* result = (char*)malloc((count)+1);
+	for (int i = 0; i < count; i++)
+	{
+		result[i] = s[pos + i];
+	}
+	result[count] = 0;
+	return result;
+}
+
+int strcmp_new(char* s1, char* s2)
+{
+
+	while (*s1 != NULL && *s2 != NULL)
+	{
+		if (*s1 < *s2)
+		{
+			return -1;
+		}
+		else if (*s1 > *s2)
+		{
+			return 1;
+		}
+		s1++;
+		s2++;
+	}
+	return 0;
+}
+//----------------
+int StrLen(char* str)
+{
+	int res = 0;
+	while (str[res++] != '\0');
+	return res;
+}
+
+
+
+int main(int argc, char** argv)
+{
+	char s1[256];
+	char s2[256];
+	int option = 9;
+	while (option != 0)
+	{
+		printf("What do you want to do?\n");
+		printf("1--> strcat\n");
+		printf("2--> strstr\n");
+		printf("3--> substr\n");
+		printf("4--> strcmp\n");
+		printf("5--> strmult\n");
+		printf("0--> Exit\n");
+		scanf("%d", &option);
+		switch (option)
+		{
+		case 1:
+		{
+			printf("Please, enter 2 strings:\n");
+			scanf("%s", &s1);
+			scanf("%s", &s2);
+			printf("%s\n", strcat_new(s1, s2));
+			break;
+		}
+		case 2:
+		{
+			printf("Please, enter 2 strings:\n");
+			scanf("%s", &s1);
+			scanf("%s", &s2);
+			printf("%s\n", strstr_new(s1, s2));
+			break;
+		}
+		case 3:
+		{
+			printf("Please, enter 1 string:\n");
+			scanf("%s", &s1);
+			printf("Pos and count:\n");
+			int pos = 0;
+			int count = 0;
+			scanf("%d", &pos);
+			scanf("%d", &count);
+			printf("%s\n", substr_new(s1, pos, count));
+			break;
+		}
+		case 4:
+		{
+			printf("If s1=s2: return 0. If s1<s2: return -1. If s1>s2: return 1\n");
+			printf("Please, enter 2 strings:\n");
+			scanf("%s", &s1);
+			scanf("%s", &s2);
+			printf("%d\n", strcmp_new(s1, s2));
+			break;
+		}
+		case 5:
+		{
+			int n = 0;
+			printf("Please, enter 1 string:\n");
+			scanf("%s", &s1);
+			printf("Please, enter multiplier :\n");
+			scanf("%d", &n);
+			printf("%s\n", strmult_new(s1, n));
+			break;
+		}
+		case 0:
+			return 0;
+		default:
+			printf("You are wrong");
+		}
+	}
+
+	return 0;
+}
---
+++ 2023.12.25-homework-7/task2/task2.cpp
@@ -0,0 +1,74 @@
+﻿#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+
+bool isLetter(char c)
+{
+	return (c >= 'а' && c <= 'я') || (c >= 'А' && c <= 'Я') ||
+		(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+bool isSyntax(char c)
+{
+	return (c == '.' || c == ',' ||
+		c == ';' || c == ':' ||
+		c == '!' || c == '?' || c == '\t');
+}
+
+int main()
+{
+	FILE* fp, * out;
+	char ch;
+
+	if ((fp = fopen("in.txt", "r")) == NULL) {
+		printf("Невозможно открыть файл для чтения");
+		exit(1);
+	}
+
+	int len_word = 0;//длина слова, отслеживаем двойной пробел
+	std::string word;//помещаем слово
+	int pos = 0;
+	std::map< std::string, int > map;
+	while (!feof(fp)) {
+		ch = getc(fp);
+
+		if ((ch == ' ') || isSyntax(ch)) {
+			if (len_word != 0) {
+				len_word = 0;
+				map[word]++;
+				word.clear();
+			}
+		}
+		else {
+			len_word++;
+			word = word + ch;
+		}
+	}
+	if (len_word > 0) { //обрабатываем последнее слово в файле
+		ch = getc(fp);
+
+		if (((ch == ' ') || (isSyntax(ch))) && (len_word != 0)) {
+
+			len_word = 0;
+			++map[word];
+			word.clear();
+
+		}
+	}
+	std::string max_word;
+	int max_count = 0;
+	for (const auto& [word, count] : map) {
+		std::cout << word << "---" << count << std::endl;
+		if (count > max_count) {
+			max_word = word;
+			max_count = count;
+		}
+	}
+	std::cout << "The most popular: " << max_word << "---" << max_count;
+	fclose(fp);
+}
+
+
+
